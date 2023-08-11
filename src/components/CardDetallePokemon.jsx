d56688ed92649b463b48a5d9539ab531175a1c13
rogu25 <ryavar.montesinos@gmail.com>
2023-08-11 12:07:29 -0500

    actualizando la repo

--- src/components/CardDetallePokemon.jsx
+++ src/components/CardDetallePokemon.jsx
@@ -77,6 +77,7 @@ function CardDetallePokemon() {
     }
 
     const onClickDeletePokemon = (e) => {
+        if (id.length !== 36) return alert("no puedes eiminar un Pokemon de la api");
         if(window.confirm("Seguro que deseas eliminar este pokemon") === true){
             dispatch(delete_pokemon(id));
             window.location.replace("/home");
